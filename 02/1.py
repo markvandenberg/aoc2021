2aa413656514ab4cdab6bccebcf3a38751973b68
Mark van den Berg <markvdb@delta-n.nl>
2021-12-02 11:51:43 +0100

    refactor 02

--- 02/1.py
+++ 02/1.py
@@ -1,9 +1,7 @@
-with open("./02/1.txt", "r") as file1:
-    values = [line.split() for line in file1.readlines()]
-
-    forward = sum([int(i[1]) for i in values if i[0] == 'forward'])
-    down = sum([int(i[1]) for i in values if i[0] == 'down']) - sum([int(i[1]) for i in values if i[0] == 'up'])
-
-    position = forward * down
-    print(position)
+with open("./02/1.txt", "r") as data:
+    values = [[c, int(x)] for c, x in [line.split() for line in data.readlines()]]
     
+    forward = sum([i[1] for i in values if i[0] == 'forward'])
+    down = sum([i[1] for i in values if i[0] == 'down']) - sum([i[1] for i in values if i[0] == 'up'])
+
+    print(forward * down)
--- 02/2.py
+++ 02/2.py
@@ -1,17 +1,16 @@
 with open("./02/2.txt", "r") as data:
-    values = [line.split() for line in data.readlines()]
+    values = [[c, int(x)] for c, x in [line.split() for line in data.readlines()]]
 
     forward = 0
     depth = 0
     aim = 0
     for value in values:
         if value[0] == 'forward':
-            forward += int(value[1])
-            depth += aim * int(value[1])
-        if value[0] == 'down':
-            aim += int(value[1])
-        if value[0] == 'up':
-            aim -= int(value[1])
+            forward += value[1]
+            depth += aim * value[1]
+        elif value[0] == 'down':
+            aim += value[1]
+        else:
+            aim -= value[1]
 
-    position = forward * depth
-    print(position)
+    print(forward * depth)
